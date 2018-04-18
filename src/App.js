dfd291e69453a7a953c41fa115beaf04ca575763
Alanna Risse <angrypirate@gmail.com>
2018-04-17 18:04:09 -0700

    did something? cant remember

--- src/App.js
+++ src/App.js
@@ -12,31 +12,29 @@ class App extends Component {
           <h1 className="App-title">Welcome to AppyHour</h1>
         </header>
 
-<main role="main">
+        <main role="main">
+          <div className="row">
+            <div className="col-12">
+              <h2>Really Fantastic Bar</h2>
 
-    <div className="row">
-      <div className="col-12">
-        <h2>Really Fantastic Bar</h2>
+              <ul>
+                <li><strong>Location</strong>: 666 Sesame Street</li>
+                <li><strong>Hours</strong>: 3-7pm M-F, 2-6 Sa-Su</li>
+              </ul>
 
-        <ul>
-          <li><strong>Location</strong>: 666 Sesame Street</li>
-          <li><strong>Hours</strong>: 3-7pm M-F, 2-6 Sa-Su</li>
-        </ul>
+              <h3>Description</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer elementum laoreet odio vel porttitor. Nullam a rutrum velit. Curabitur ullamcorper faucibus ullamcorper. Nunc est arcu, dapibus ut enim in, malesuada placerat ligula. Donec et mollis tortor. Nunc eget ex molestie, dignissim sapien ut, mollis purus. Donec et dolor eget ex congue tristique. In convallis est quis orci sagittis sagittis.</p>
+            </div>
+          </div>
+          <div className="row page-nav">
+            <div className="col-6 text-left"><i className="fas fa-arrow-alt-circle-left fa-3x"></i></div>
+            <div className="col-6 text-right"><i className="fas fa-arrow-alt-circle-right fa-3x"></i></div>
+          </div>
 
-        <h3>Description</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer elementum laoreet odio vel porttitor. Nullam a rutrum velit. Curabitur ullamcorper faucibus ullamcorper. Nunc est arcu, dapibus ut enim in, malesuada placerat ligula. Donec et mollis tortor. Nunc eget ex molestie, dignissim sapien ut, mollis purus. Donec et dolor eget ex congue tristique. In convallis est quis orci sagittis sagittis.</p>
-
-      </div>
-    </div>
-    <div className="row page-nav">
-      <div className="col-6 text-left"><i className="fas fa-arrow-alt-circle-left fa-3x"></i></div>
-      <div className="col-6 text-right"><i className="fas fa-arrow-alt-circle-right fa-3x"></i></div>
-    </div>
-
-  </main>
-  <footer>
-    <p>Footery things</p>
-  </footer>
+        </main>
+        <footer>
+          <p>Footery things</p>
+        </footer>
       </div>
     );
   }
