1995d68af92e43abdf5d7a339a4d66e075bb4b2c
christophergoot <christophergoot@gmail.com>
2018-08-16 15:24:31 -0700

    location details page wireframe

---
+++ src/components/detail/mock-data.js
@@ -0,0 +1,14 @@
+export const MOCK_DETAIL_DATA = {
+	name: 'Really Fantastic Bar',
+	image: 'https://via.placeholder.com/500x500',
+	address: '666 Sesame Street',
+	neighborhood: 'NW',
+	phone: '503-898-1234',
+	hours: '3-7pm M-F, 2-6 Sa-Su',
+	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer elementum laoreet odio vel porttitor. Nullam a rutrum velit. Curabitur ullamcorper faucibus ullamcorper. Nunc est arcu, dapibus ut enim in, malesuada placerat ligula. Donec et mollis tortor. Nunc eget ex molestie, dignissim sapien ut, mollis purus. Donec et dolor eget ex congue tristique. In convallis est quis orci sagittis sagittis.',
+	links: {
+		website: 'http://thenestbarever.com',
+		instagram: '@thebestbarpdx',
+		facebook: '@thebestbarpdx'
+	}
+};
