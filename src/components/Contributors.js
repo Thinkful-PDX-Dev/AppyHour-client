e0ea2dd860f316af0d7d8e6e61c9cc56e673692d
Alanna Risse <angrypirate@gmail.com>
2018-08-23 15:52:08 -0700

    added text

--- src/components/Contributors.js
+++ src/components/Contributors.js
@@ -4,6 +4,9 @@ class Contributors extends Component {
 	render() {
 		return (
 			<div>
+				<h2>About AppyHour</h2>
+				<p>This app was conceived, created and maintained by the Portland students of <a href="http://thinkful.com/">Thinkful</a>, an online code school.</p>
+		
 				<h3>Contributors</h3>
 				<p>are super awesome</p>
 			</div>
